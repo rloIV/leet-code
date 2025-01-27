e57507421abb744c5b325231b773f122a211f15f
rlo <123877280+rloIV@users.noreply.github.com>
2025-01-27 18:07:48 +0100

    Create 66. Plus One.cs

---
+++ 66. Plus One.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        for (int i = digits.Length - 1; i >= 0; i--) // i>=0 aby iterować od końca
+        {
+            if (!(digits[i] == 9))
+            {
+                digits[i]++;
+                return digits;
+            }
+            else
+            {
+                digits[i] = 0;
+            }
+            
+        }
+        int[] result = new int[digits.Length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
